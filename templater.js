2ae6b2612968a6f86a9d5f8f5f1b4fcd7b5fb217
flaviozorzetto <flaviozorzetto11@gmail.com>
2023-12-17 12:02:03 -0300

    finished main workflow

--- templater.js
+++ templater.js
@@ -1,28 +1,127 @@
+const PizZip = require('pizzip');
+const Docxtemplater = require('docxtemplater');
 const prompt = require('prompt-sync')();
 const fs = require('fs');
 const path = require('path');
 
-let validDocxFilePaths = [];
-const docxRegexMatcher = /(\.)(\w*)$/g;
+let inputFolderName = 'input';
+let outputFolderName = 'output';
+let payloadFolderName = 'payload';
 
-function executeProgram() {
+function createFolder(folderName) {
+	let directoryContent = fs.readdirSync(__dirname);
+	if (!directoryContent.filter(f => f == folderName).length == 1) {
+		fs.mkdirSync(folderName);
+	}
+}
+
+function ensureNecessaryFilesExists() {
+	createFolder(inputFolderName);
+	createFolder(outputFolderName);
+	createFolder(payloadFolderName);
+
+	let payloadFolderContent = fs.readdirSync(
+		path.resolve(__dirname, payloadFolderName)
+	);
+
+	if (payloadFolderContent.filter(f => f == 'data.txt').length == 0) {
+		throw new Error(
+			"Erro: nenhum arquivo 'data.txt' foi encontrado dentro da pasta de payload"
+		);
+	}
+}
+
+function getFilesList() {
+	let validDocxFilePaths = [];
+	const docxRegexMatcher = /(\.)(\w*)$/g;
 	const files = fs.readdirSync(path.resolve(__dirname, 'input'));
 
-	files.forEach(file => {
+	files.forEach((file, index) => {
 		let match = file.match(docxRegexMatcher)[0];
 		if (match == '.docx') {
 			const validDocxFilePath = path.resolve(__dirname, 'input', file);
-			validDocxFilePaths.push({ fileName: file, filePath: validDocxFilePath });
+			validDocxFilePaths.push({
+				fileName: file,
+				filePath: validDocxFilePath,
+				index,
+			});
 		}
 	});
 
 	if (validDocxFilePaths.length == 0) {
 		throw new Error(
-			'Não existe nenhum arquivo do tipo .docx na pasta de input'
+			'Erro: não existe nenhum arquivo do tipo .docx na pasta de input, insira um valido e execute de novo o programa'
 		);
 	}
 
-	console.log(validDocxFilePaths);
+	return validDocxFilePaths;
+}
+
+function throwInvalidNumberException() {
+	throw new Error('Erro: selecione um número válido');
+}
+
+function selectFileFromList(files) {
+	let message = 'Selecione um dos arquivos a serem formatados: ';
+
+	files.forEach(file => {
+		message += `${file.index} - ${file.fileName}; `;
+	});
+
+	let promptInput = prompt(message);
+	if (promptInput.length == 0) {
+		throwInvalidNumberException();
+	}
+
+	let selectedInput = Number(promptInput);
+	if (isNaN(selectedInput)) {
+		throwInvalidNumberException();
+	}
+
+	let fileToReturn = files[selectedInput];
+	if (!fileToReturn) {
+		throwInvalidNumberException();
+	}
+
+	return fileToReturn;
+}
+
+function generateOutputFile(file) {
+	let payloadData = fs
+		.readFileSync(path.resolve(__dirname, payloadFolderName, 'data.txt'))
+		.toString();
+	try {
+		let payloadJsonData = JSON.parse(payloadData);
+		const docxContent = fs.readFileSync(file.filePath, 'binary');
+		const zipDocx = new PizZip(docxContent);
+		const doc = new Docxtemplater(zipDocx, {
+			paragraphLoop: true,
+			linebreaks: true,
+		});
+		doc.render(payloadJsonData);
+		const buf = doc.getZip().generate({
+			type: 'nodebuffer',
+			compression: 'DEFLATE',
+		});
+		fs.writeFileSync(
+			path.resolve(__dirname, outputFolderName, file.fileName),
+			buf
+		);
+	} catch (error) {
+		throw new Error(error);
+		// 'Erro: não foi possivel converter o arquivo para um JSON válido, garanta que o arquivo data.txt tenha um formato válido seguindo a seguinte estrutura: { "nome_da_variavel": "valor_da_variavel", "nome_da_variavel_2", "valor_da_variavel_2" }, site de referencia: https://support.oneskyapp.com/hc/en-us/articles/208047697-JSON-sample-files'
+	}
+}
+
+function executeProgram() {
+	try {
+		ensureNecessaryFilesExists();
+		let files = getFilesList();
+		let selectedFile = selectFileFromList(files);
+		generateOutputFile(selectedFile);
+	} catch (error) {
+		prompt(error.message);
+	}
 }
 
 executeProgram();
